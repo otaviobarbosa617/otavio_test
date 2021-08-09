4bbb5c6db3b3751cd1695f442d28d64688530c18
otaviobarbosa617 <59937699+otaviobarbosa617@users.noreply.github.com>
2021-08-09 16:47:38 -0400

    blog changes

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby'
+import { Container } from "react-bootstrap"
 
 
 const BlogPage = ({data}) => (
   <Layout>
-    <SEO title="Blog" />  
+    <SEO title="Blog" /> 
+    <Container>
     <h1>Latest Posts</h1>
     {data.allMarkdownRemark.edges.map(post => (
         <div key={ post.node.id }>
             <br></br>
             <h3>{post.node.frontmatter.title}</h3>
-            <small>Post by {post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
-            <br />
-            <br/>
+            <p>Post by {post.node.frontmatter.author} on {post.node.frontmatter.date}</p>
             <Link to={post.node.frontmatter.path}>Read More</Link>
         </div>
     ))}
+    </Container> 
   </Layout>
 )
 
--- src/pages/blog/testst.md
+++ src/pages/blog/testst.md
@@ -1,6 +1,7 @@
 ---
-path: /src/pages/postest
+path: /postest
 date: "2021-08-09"
 title: Testst
+author: Otavio Barbosa
 ---
 Test today
